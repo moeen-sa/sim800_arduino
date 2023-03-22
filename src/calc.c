de328bd4bc6028cd5682a8487bdfe072b115d5e7
moeen-sa <moeensaberi@gmail.com>
2023-03-22 17:52:36 +0330

    Create calc.c

---
+++ src/calc.c
@@ -0,0 +1,14 @@
+#include "calc.h"
+int calc::sum(int a, int b){
+return (a+b);
+}
+int calc::diff(int c, int d){
+return (c-d);
+}
+
+int calc::div(int e, int f){
+return (e/f);
+}
+int calc::mul(int g, int h){
+return (g*h);
+}
